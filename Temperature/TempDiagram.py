5152815df594d2bae6ed1248c6825b235a970431
IseeJ <122057875+IseeJ@users.noreply.github.com>
2024-06-24 14:35:28 -0400

    Update TempDiagram.py

--- Temperature/TempDiagram.py
+++ Temperature/TempDiagram.py
@@ -21,7 +21,26 @@ class Worker(QThread):
         self.is_running = True
         logging.info("Serial Start")
         self.start_time = None
-
+    def hex_dec(T_hex):
+        try:
+            T_val = int(T_hex, 16)
+            T_max = 18000  # 1800C is max, use as threshold to check that the hex is neg
+            hex_max = 0xFFFF  # FFFF max
+            if T_val > T_max:
+                T = -(hex_max - T_val + 1) / 10  # handling negative value
+            else:
+                T = T_val / 10
+            return T
+        except ValueError:
+            return 'err'
+    def parse_temp(response):
+        response_hex = response.hex()
+        temperatures = []
+        for i in range(8):
+            hex_str = response_hex[34+i*4:36+i*4] + response_hex[32+i*4:34+i*4]
+            temperatures.append(hex_dec(hex_str))
+        return tuple(temperatures)
+        
     def run(self):
         try:
             hex_data = [0x01, 0x16, 0x7B, 0x28, 0x48, 0x4C, 0x45, 0x48, 0x54, 0x43, 0x34, 0x30, 0x39, 0x35, 0x67, 0x71, 0x29, 0x7D, 0x7E, 0x04]
@@ -53,27 +72,6 @@ class Worker(QThread):
         self.is_running = False
         self.quit()
         self.wait()
-
-    def hex_dec(T_hex):
-        try:
-            T_val = int(T_hex, 16)
-            T_max = 18000  # 1800C is max, use as threshold to check that the hex is neg
-            hex_max = 0xFFFF  # FFFF max
-            if T_val > T_max:
-                T = -(hex_max - T_val + 1) / 10  # handling negative value
-            else:
-                T = T_val / 10
-            return T
-        except ValueError:
-            return 'err'
-
-    def parse_temp(response):
-        response_hex = response.hex()
-        temperatures = []
-        for i in range(8):
-            hex_str = response_hex[34+i*4:36+i*4] + response_hex[32+i*4:34+i*4]
-            temperatures.append(hex_dec(hex_str))
-        return tuple(temperatures)
 
 class PurifierModel(QObject):
     def __init__(self, parent=None):
